fe33a95ba2badaf0d2aa2f008b5b01f17850bbf4
Erik McClure <erikm@blackspherestudios.com>
2017-03-17 23:39:32 -0700

    Reconstruct directories, use vcpkg, use nested RB trees for scene graph.

--- include/magnesium/mg_dec.h
+++ include/magnesium/mg_dec.h
@@ -4,17 +4,21 @@
 #ifndef __DEC_H__MG__
 #define __DEC_H__MG__
 
-#include "bss-util\bss_compiler.h"
-
 #define MG_VERSION_MAJOR 0
 #define MG_VERSION_MINOR 1
 #define MG_VERSION_REVISION 0
+
+#ifndef _WINRESRC_
+
+#include "bss-util\bss_compiler.h"
 
 #if defined(MAGNESIUM_EXPORTS) || defined(MG_STATIC_LIB)
 #pragma warning(disable:4251)
 #define MG_DLLEXPORT BSS_COMPILER_DLLEXPORT
 #else
 #define MG_DLLEXPORT BSS_COMPILER_DLLIMPORT
+#endif
+
 #endif
 
 #endif
